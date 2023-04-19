414bdc77c06fe5468f0f1c5ac74d8af26232e172
sisi.miao <qf1990@kit.edu>
2023-04-19 18:28:40 +0200

    implementation

---
+++ README.md
@@ -0,0 +1,13 @@
+# Neural Belief Propagation Decoding of Quantum LDPC Codes Using Overcomplete Check Matrices
+## Sisi Miao, Alexander Schnerring, Haizheng Li, and Laurent Schmalen
+
+This jupyter notebook provides the implementation which produces the results in [1]. In specific, this notebook contains:
+* The neural BP(NBP) decoder
+* The function for training, can be run on CPU or GPU
+* Call on the C++ functions which evaluate the trained decoder
+* Addtionally, the overcomplete check matrices used in [1] are provided in the ./PCM folder
+
+[1] S. Miao, A. Schnerring, H. Li and L. Schmalen, "Neural belief propagation decoding of quantum LDPC codes using overcomplete check matrices," Proc. IEEE Inform. Theory Workshop (ITW), Saint-Malo, France, Apr. 2023, https://arxiv.org/abs/2212.10245
+
+## abstract:
+The recent success in constructing asymptotically good quantum low-density parity-check (QLDPC) codes makes this family of codes a promising candidate for error-correcting schemes in quantum computing. However, conventional belief propagation (BP) decoding of QLDPC codes does not yield satisfying performance due to the presence of unavoidable short cycles in their Tanner graph and the special degeneracy phenomenon. In this work, we propose to decode QLDPC codes based on a check matrix with redundant rows, generated from linear combinations of the rows in the original check matrix. This approach yields a significant improvement in decoding performance with the additional advantage of very low decoding latency. Furthermore, we propose a novel neural belief propagation decoder based on the quaternary BP decoder of QLDPC codes which leads to further decoding performance improvements.
---
+++ stabilizerCodes.h
@@ -0,0 +1,82 @@
+//
+// Created by sisi on 1/13/23.
+//
+#ifndef BPDECODING_STABILIZIERCODES_H
+#define BPDECODING_STABILIZIERCODES_H
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include<random>
+#include <functional>
+#include <assert.h>
+enum class stabilizerCodesType{
+    GeneralizedBicycle = 0,
+    HpergraphProduct = 1,
+    toric = 3
+};
+
+class stabilizerCodes {
+public:
+    stabilizerCodes(unsigned n, unsigned k, unsigned m,stabilizerCodesType codeType, bool trained = false);
+
+    std::vector<bool> decode(unsigned int L, double epsilon);
+
+    std::vector<bool>
+    floodingDecode(unsigned int L, double epsilon, const std::vector<unsigned> error);
+
+    std::vector<bool> checkSucess(const double *Taux, const double *Tauy, const double *Tauz);
+    void loadWeights_cn();
+    void loadWeights_vn();
+    void loadWeights_llr();
+    void readH();
+    void readG();
+    inline unsigned traceInnerProduct(unsigned a, unsigned b);
+    bool checksymplectic();
+
+    void addErrorGivenEpsilon(double epsilon);
+
+    void addErrorGivenPositions(int pos[], int error[], int size);
+
+    void calSyndrome(); //also check if the error is all 0, if true, not decoding needed
+
+    double bielief_quantize(double Taux, double Tauy, double Tauz);
+
+
+    bool print_msg = false;
+
+
+
+    stabilizerCodesType mycodetype;
+    unsigned N;
+    unsigned K;
+    unsigned M;
+    unsigned G_rows;
+    bool mTrained;
+    unsigned trained_iter;
+    std::vector<unsigned> error;
+    std::vector<unsigned> error_hat;
+    std::vector<unsigned> syn;
+    std::vector<std::string> errorString;
+    std::vector<std::string> errorHatString;
+    unsigned maxDc;
+    unsigned maxDv;
+    std::vector<unsigned> dv;
+    std::vector<unsigned> dc;
+    std::vector<std::vector<unsigned>> Nv;
+    std::vector<std::vector<unsigned>> Mc;
+    std::vector<std::vector<unsigned>> checkVal;
+    std::vector<std::vector<unsigned>> varVal;
+
+    std::vector<std::vector<unsigned>> Nvk;
+    std::vector<std::vector<unsigned>> Mck;
+
+    std::vector<std::vector<unsigned>> G;
+
+    std::vector<std::vector<std::vector<double>>> weights_cn;
+    std::vector<std::vector<std::vector<double>>> weights_vn;
+    std::vector<std::vector<double>> weights_llr;
+
+};
+#endif //BPDECODING_STABILIZIERCODES_H
